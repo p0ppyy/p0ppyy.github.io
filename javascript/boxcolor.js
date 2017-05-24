be9772f809e9b77ee1cb2303d15cf89886f9693e
p0ppyy <maksimilian.andersen@gmail.com>
2017-05-24 21:58:37 +0200

    Add files via upload

--- javascript/boxcolor.js
+++ javascript/boxcolor.js
@@ -3,18 +3,17 @@ $('.project-box').each(function(index, element) {
 	var pastel = 'hsl(' + hue + ', 100%, 87.5%)';
     $(this).css("background-color", pastel);
 });
-$('.project-box').each(function(index, element) {
-	if(document.width() > 1100){
-	$(this).hover(function(){
+
+if($("body").width() > 1100){
+	$(".project-box").hover(function(){
 		$(this).animate({opacity: 1}, 300);
 		$(this).children(".project-head").css("display", "none");
 		$(this).children(".project-desc").css("display", "table-cell");
 	},function(){
 		$(this).animate({opacity: 0.5}, 300)
-		$(".project-head").css("display", "table-cell");
-		$(".project-desc").css("display", "none");
 		$(this).children(".project-head").css("display", "table-cell");
 		$(this).children(".project-desc").css("display", "none");
 	})
-	}
-});
+
+
+}
